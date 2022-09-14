ec4f50a4159d23c04f85ee403fd3a6dbe8c7a52a
Chachel <alexgb11@gmail.com>
2022-09-14 13:53:01 +0200

    Initial commit

---
+++ src/main/java/es/akka/backenddevtestakka/BackendDevTestAkkaApplication.java
@@ -0,0 +1,13 @@
+package es.akka.backenddevtestakka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevTestAkkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDevTestAkkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/akka/backenddevtestakka/BackendDevTestAkkaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.akka.backenddevtestakka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevTestAkkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
