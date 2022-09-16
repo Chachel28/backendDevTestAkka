89e5129a851366dbec2b708076998ef9c74b9d7b
Chachel <alexgb11@gmail.com>
2022-09-17 00:39:15 +0200

    Changed exceptions for different types of errors

--- src/main/java/es/akka/backenddevtestakka/services/ExistingApisService.java
+++ src/main/java/es/akka/backenddevtestakka/services/ExistingApisService.java
@@ -1,7 +1,10 @@
 package es.akka.backenddevtestakka.services;
 
 import es.akka.backenddevtestakka.dto.ProductDetailDto;
+import es.akka.backenddevtestakka.exceptions.ProductInternalServerException;
 import es.akka.backenddevtestakka.exceptions.ProductNotFoundException;
+import es.akka.backenddevtestakka.exceptions.ProductTimeOutException;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
@@ -9,11 +12,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
+@Log
 public class ExistingApisService {
     @Value("${existingApis.url}")
     private String existingApisUrl;
@@ -24,35 +29,51 @@ public class ExistingApisService {
     @Value("${existingApis.paths.get-product-productId}")
     private String pathProductIds;
 
-    WebClient webClient = WebClient.create(existingApisUrl);
+    WebClient webClient = WebClient.create();
 
-    private ProductDetailDto getProductById(String id) throws ProductNotFoundException{
-        String endpoint = pathProductIds.replace("{productId}", id);
+    private ProductDetailDto getProductById(String id) throws ProductNotFoundException {
 
-        Mono<ProductDetailDto> response = webClient.get()
-                .uri(endpoint)
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(existingApisUrl+pathProductIds)
+                        .build(id)
+                )
                 .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> clientResponse.bodyToMono(ProductDetailDto.class).flatMap(error -> Mono.error(new ProductNotFoundException(id))))
-                .bodyToMono(ProductDetailDto.class);
-
-        return response.block();
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class).flatMap(error -> Mono.error(new ProductNotFoundException(id))))
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> clientResponse.bodyToMono(String.class).flatMap(error -> Mono.error(new ProductInternalServerException())))
+                .bodyToMono(ProductDetailDto.class)
+                .timeout(Duration.ofSeconds(5))
+                .doOnError(ProductTimeOutException.class, Throwable::printStackTrace)
+                .block();
     }
 
-    public List<ProductDetailDto> getSimilarProducts(String id) throws ProductNotFoundException{
-        String endpoint = pathSimilarIds.replace("{productId}", id);
+    private List<String> getSimilarProductsIdList(String id) {
 
-        Mono<List<String>> response = webClient.get()
-                .uri(endpoint)
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(existingApisUrl+pathSimilarIds)
+                        .build(id)
+                )
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {});
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class).flatMap(error -> Mono.error(new ProductNotFoundException(id))))
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> clientResponse.bodyToMono(String.class).flatMap(error -> Mono.error(new ProductInternalServerException())))
+                .bodyToMono(new ParameterizedTypeReference<List<String>>() {})
+                .timeout(Duration.ofSeconds(5))
+                .doOnError(ProductTimeOutException.class, Throwable::printStackTrace)
+                .block();
+    }
 
-        List<String> ids = response.block();
+    public List<ProductDetailDto> getSimilarProducts(String id) throws ProductNotFoundException {
+
+        List<String> ids = getSimilarProductsIdList(id);
         List<ProductDetailDto> similarProducts = new ArrayList<>();
+
         if (ids != null) {
-            for(String productId: ids){
+            for (String productId : ids) {
                 similarProducts.add(getProductById(productId));
             }
         }
+
         return similarProducts;
     }
 }
