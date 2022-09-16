cc8ba5ccb3bae0be479439742f2dfe4bfbf1d936
Chachel <alexgb11@gmail.com>
2022-09-16 21:34:21 +0200

    Custom Exception created

---
+++ src/main/java/es/akka/backenddevtestakka/exceptions/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package es.akka.backenddevtestakka.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Product not found")
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super("Product with ID: " + message + " not found");
+    }
+
+}
